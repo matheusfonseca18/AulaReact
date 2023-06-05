159d036111c99c157804aa3a6e3a745d70e22942
matheusfonseca18 <matheus.fonseca0218@gmail.com>
2023-06-05 09:26:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá Mundo!</h1>
+      <p>Tô escrevendo aqui pra testar</p>
+      <div className="test">
+        <h4>Título de teste</h4>
+        
+      </div>
     </div>
   );
 }
