4d01aebd2efd9dbe2ab1018712591737eafb6588
matheusfonseca18 <matheus.fonseca0218@gmail.com>
2023-06-12 11:42:33 -0300

    reactzada

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import './Components/style.css';
 import PrimeiroComponente from './Components/PrimeiroComponente';
 import SegundoComponente from './Components/SegundoComponente';
-import ComponenteImagem from './Components/ComponenteImagem'
+import ComponenteImagem from './Components/ComponenteImagem';
+import Componente3 from './Components/Componente3';
+
 //link de img
 import gato2 from './Assets/gato2.jpg'
 let gatoLink = <img src="https://www.petsupport.com.br/wp-content/uploads/2021/09/necessidades-dos-gatos-1-1024x576.jpg" alt="gato3" width="300"/>
 
 function App() {
+
   let nome = "Matheus";
+
   const RandomMsg = () => {
     return console.log ("Cliquei no botão")
-  }
+  };
+
+const RandomMsg2 = () => {
+  let Paragra = document.querySelector (`.Muda`)
+  return Paragra.innerHTML = (`ebaa`)
+};
+
+
+
+let nota = document.querySelector (`input`);
+
+const RandomMostra = () => {
+  let mostra = document.querySelector (`.showw`)
+  return mostra.innerHTML = (nota)
+}
 
   return (
     <div className="App">
@@ -29,6 +47,16 @@ function App() {
       <div>
         <h1>eventos</h1>
         <button onClick={RandomMsg}>cs.log</button>
+        <button onClick={RandomMsg2}>Clica aí</button>
+        <p className="Muda">0</p>
+        <Componente3/>
+        <p style={{color: "black"}}>CSS inline</p>
+
+        <input type="palavra" />
+        <button onClick={RandomMostra}>mostra</button>
+
+        <p className = "showw" style={nota >= 6 ? {color: "green"} : {color: "red"}}>0</p>
+
       </div>
     </div>
   );
---
+++ src/Components/Componente3.jsx
@@ -0,0 +1,12 @@
+import './Componente3.css';
+
+const Componente3 = () => {
+  return (
+    <div><h3>Componete3</h3>
+        <p className='comp3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae assumenda quisquam, a officiis vitae perferendis temporibus error. Aliquid eaque reiciendis dolorum natus accusantium. Enim iure iusto perspiciatis corrupti, excepturi nisi!
+        </p>
+    </div>
+  )
+}
+
+export default Componente3;
